e64de9f349949b368738e25bda129e4267fcee19
Alex Jones <ajonzy@gmail.com>
2022-06-07 19:15:38 -0600

    Added year, lecture_time, and clinical_time columns to event table

--- app.py
+++ app.py
@@ -19,18 +19,24 @@ class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     month = db.Column(db.String, nullable=False, unique=False)
     start_date = db.Column(db.Integer, nullable=False, unique=False)
+    year = db.Column(db.Integer, nullable=False, unique=False)
+    lecture_time = db.Column(db.String, nullable=False, unique=False)
+    clinical_time = db.Column(db.String, nullable=False, unique=False)
     signups = db.Column(db.Integer, nullable=False, unique=False)
     
-    def __init__(self, month, start_date):
+    def __init__(self, month, start_date, year, lecture_time, clinical_time):
         self.month = month
         self.start_date = start_date
+        self.year = year
+        self.lecture_time = lecture_time
+        self.clinical_time = clinical_time
         self.signups = 0
         
 
 # Marshmallow Schemas
 class EventSchema(ma.Schema):
     class Meta:
-        fields = ("id", "month", "start_date", "signups")
+        fields = ("id", "month", "start_date", "year", "lecture_time", "clinical_time", "signups")
 
 event_schema = EventSchema()
 multiple_event_schema = EventSchema(many=True)
@@ -53,9 +59,12 @@ def add_event():
 
     data = request.get_json()
     month = data.get("month")
+    year = data.get("year")
+    lecture_time = data.get("lecture_time")
+    clinical_time = data.get("clinical_time")
     start_date = data.get("start_date")
 
-    record = Event(month, start_date)
+    record = Event(month, start_date, year, lecture_time, clinical_time)
     db.session.add(record)
     db.session.commit()
 
@@ -77,6 +86,9 @@ def update_event(id):
     data = request.get_json()
     month = data.get("month")
     start_date = data.get("start_date")
+    year = data.get("year")
+    lecture_time = data.get("lecture_time")
+    clinical_time = data.get("clinical_time")
     signups = data.get("signups")
 
     record = db.session.query(Event).filter(Event.id == id).first()
@@ -84,6 +96,12 @@ def update_event(id):
         record.month = month
     if start_date is not None:
         record.start_date = start_date
+    if year is not None:
+        record.year = year
+    if lecture_time is not None:
+        record.lecture_time = lecture_time
+    if clinical_time is not None:
+        record.clinical_time = clinical_time
     if signups is not None:
         record.signups = signups
     db.session.commit()
